63db58816ebe7198b770748ed704773d01b69343
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-04-21 19:32:17 -0900

    Create 0-pascal_triangle.py

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a module used to provides the pascal triangle function.
+"""
+
+
+def pascal_triangle(n):
+    """
+    This function will return list of lists of integers
+    representing the Pascal’s triangle of n.
+    """
+
+    subList = [1]
+    result = []
+    while n > 0:
+        result.append(subList)
+        helperList = subList.copy()
+        prevNum = 0
+        subList = []
+        for nextNum in helperList:
+            subList.append(prevNum + nextNum)
+            prevNum = nextNum
+        subList.append(1)
+        n -= 1
+
+    return result
