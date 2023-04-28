58c604089929cfcb7fca10024c0530db25c98b8e
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-04-27 15:11:52 -0900

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you.
-Each box is numbered sequentially
-from 0 to n - 1 and each box may
-contain keys to the other boxes.
+This modeule supplies the function `canUnlockAll` which
+analyses a list of lockboxes
 """
 
 
 def canUnlockAll(boxes):
     """
-     a method that determines if all the boxes can be opened.
-    :param boxes:
-    :return: True or False
+    This function checks and unlocks other locked boxes
     """
     if not boxes or type(boxes) is not list:
         return False
