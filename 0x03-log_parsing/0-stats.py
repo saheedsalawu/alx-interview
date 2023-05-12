6018bcdaa83af4a95fa63014d8e606f05651417f
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-05-11 19:52:20 -0900

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,79 @@
 #!/usr/bin/python3
-"""reads stdin line by line and computes metrics:"""
 
+"""
+Log Parsing
+
+"""
+import re
 import sys
+import random
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+fileSize = 0
+i = 0
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
 
-except Exception as err:
-    pass
+def print_log(fileSize, statusCodes, responses):
+    """
+    This function prints the log.
+    """
+    print('File size: {}'.format(fileSize))
+    for code in statusCodes:
+        if responses[code] > 0:
+            print('{}: {}'.format(code, responses[code]))
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+
+def run():
+    """
+    Log parser
+    """
+    try:
+        i = 0
+        fileSize = 0
+        responses = {
+            '200': 0,
+            '301': 0,
+            '400': 0,
+            '401': 0,
+            '403': 0,
+            '404': 0,
+            '405': 0,
+            '500': 0
+        }
+        statusCodes = ['200', '301', '400', '401', '403', '404', '405', '500']
+        for line in sys.stdin:
+            neededString = re.search(r'[2-5]0[0-5] \d+', line)
+            # ip = re.search(r'\d+.\d+.\d+.\d+', line)
+            if neededString is None:
+                continue
+            neededString = neededString.group()
+            values = neededString.split(' ')
+            status = values[0]
+            size = values[1]
+            try:
+                # status = int(status)
+                size = int(size)
+                # status = str(status)
+                size = str(size)
+            except Exception:
+                continue
+            if status in statusCodes:
+                responses[status] += 1
+            try:
+                fileSize += int(size)
+            except Exception:
+                pass
+            if i == 9:
+                i = 0
+                print_log(fileSize, statusCodes, responses)
+                continue
+            i += 1
+        print_log(fileSize, statusCodes, responses)
+    except KeyboardInterrupt as e:
+        print_log(fileSize, statusCodes, responses)
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    run()
