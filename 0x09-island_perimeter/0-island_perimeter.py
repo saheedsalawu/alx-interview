d354bb278a9c25834724d84f0c84f9f7fa2b9e37
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-06-21 01:31:08 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,97 +1,25 @@
 #!/usr/bin/python3
-
-"""
-This module provides the function `island_perimeter`
+"""Island perimeter computing module.
 """
 
 
-def island_perimeter(grid: list):
+def island_perimeter(grid):
+    """Computes the perimeter of an island with no lakes.
     """
-    Calculates and returns the perimeter of an island in `grid`
-    """
-    if grid is None:
-        return 0
-    if len(grid) == 0:
-        return 0
     perimeter = 0
-    rows = len(grid)
-    columns = len(grid[0])
-    if all([all(row) for row in grid]):
-        return 2 * (rows + columns)
-    onLand = False
-    paths_to_follow = []
-    paths_followed = []
-    i, j = 0, 0
-    while i < rows:
-        j = 0
-        while j < columns:
-
-            cell_perimeter = 0
-            if grid[i][j]:
-                paths_followed.append((i, j))
-                if i - 1 >= 0:
-                    if not grid[i - 1][j]:
-                        cell_perimeter += 1
-                    else:
-                        path = (i - 1, j)
-                        if (path not in paths_followed and
-                                path not in paths_to_follow):
-                            paths_to_follow.append(path)
-                else:
-                    cell_perimeter += 1
-                if i + 1 < rows:
-                    if not grid[i + 1][j]:
-                        cell_perimeter += 1
-                    else:
-                        path = (i + 1, j)
-                        if (path not in paths_followed and
-                                path not in paths_to_follow):
-                            paths_to_follow.append(path)
-                else:
-                    cell_perimeter += 1
-                if j - 1 >= 0:
-                    if not grid[i][j - 1]:
-                        cell_perimeter += 1
-                    else:
-                        path = (i, j - 1)
-                        if (path not in paths_followed and
-                                path not in paths_to_follow):
-                            paths_to_follow.append(path)
-                else:
-                    cell_perimeter += 1
-                if j + 1 < columns:
-                    if not grid[i][j + 1]:
-                        cell_perimeter += 1
-                    else:
-                        path = (i, j + 1)
-                        if (path not in paths_followed and
-                                path not in paths_to_follow):
-                            paths_to_follow.append(path)
-                else:
-                    cell_perimeter += 1
-            perimeter += cell_perimeter
-            if cell_perimeter >= 3:
-                if not len(paths_to_follow):
-                    onLand = True
-                if onLand and not len(paths_to_follow):
-                    return perimeter
-                else:
-                    onLand = True
-            elif not onLand:
-                perimeter -= cell_perimeter
-                paths_to_follow = []
-                if len(paths_followed):
-                    paths_followed.pop()
-            if onLand and len(paths_to_follow):
-                next = paths_to_follow.pop()
-                paths_followed = list(set(paths_followed))
-                paths_to_follow = list(set(paths_to_follow))
-                i, j = next
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
                 continue
-            onLand = False
-            if not onLand and perimeter:
-                return perimeter
-            j += 1
-        i += 1
-
-    return 0
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
